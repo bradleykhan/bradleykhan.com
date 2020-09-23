9abe6035d048bfc91e71005dca59af4c059786f8
bradleykhan <github@bradleykhan.com>
2020-09-23 02:59:23 +0100

    Make decision on colours and font sizes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,18 +1,22 @@
 const theme = {
     colours: {
-        orange: "#D38235",
-        grey: "#39444A",
-        darkgrey: "#252c30"
+        orange: "#FF8311",
+        lightgrey: "#C0CCDE",
+        darkgrey: "#1A1F22",
+        white: "#FFFFFF"
     },
     fontSizes: {
-        title: "32px",
-        subtitle: "20px",
-        text: "12px"
+        biggest: "64px",
+        bigger: "24px",
+        big: "20px",
+        medium: "16px",
+        small: "14px",
+        smallest: "12px"
     }
 }
 
-let Colours = { ...theme.colours };
-let FontSizes = { ...theme.fontSizes };
+const Colours = { ...theme.colours };
+const FontSizes = { ...theme.fontSizes };
 
 export { Colours, FontSizes };
 export default theme;
